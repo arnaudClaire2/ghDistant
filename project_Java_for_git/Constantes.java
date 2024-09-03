7b1603d4abb0abda043640df8866c3244a4b0b5d
Arnaud <arnaudclaire20@yahoo.fr>
2024-09-03 15:55:52 +0200

    first commit

---
+++ project_Java_for_git/Constantes.java
@@ -0,0 +1,5 @@
+public class Constantes {
+    // Constantes pour le titre et le texte du label
+    public static final String HELLO_WORLD_TITLE = "Title Hello World";
+    public static final String HELLO_WORLD_TEXT = "Hello World Text";
+}
---
+++ project_Java_for_git/HelloWorld.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Utilisation des constantes
+        JFrame fenetre = new JFrame(Constantes.HELLO_WORLD_TITLE);
+        fenetre.setSize(200, 200);
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel(Constantes.HELLO_WORLD_TEXT, SwingConstants.CENTER);
+        label.setOpaque(true);
+        label.setBackground(Color.YELLOW);
+
+        fenetre.add(label);
+        fenetre.setVisible(true);
+    }
+}
