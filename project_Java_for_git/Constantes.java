8c1d4451deb49518e30014ecfc2940a3206189a7
Arnaud <arnaudclaire20@yahoo.fr>
2024-09-03 15:58:04 +0200

    C2

--- project_Java_for_git/Constantes.java
+++ project_Java_for_git/Constantes.java
@@ -2,4 +2,5 @@ public class Constantes {
     // Constantes pour le titre et le texte du label
     public static final String HELLO_WORLD_TITLE = "Title Hello World";
     public static final String HELLO_WORLD_TEXT = "Hello World Text";
+    public static final String HELLO_MORE_TEXT = "Hello More Text";
 }
